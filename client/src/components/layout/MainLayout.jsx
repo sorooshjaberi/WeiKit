feb9d0360e278bca95e5418d3f4191e462745161
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-05-30 00:08:18 +0430

    current network on wallet connect

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -2,9 +2,44 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "./style.module.scss";
 import { BsLinkedin, BsGithub, BsTelegram, BsMedium } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
-
+import Web3 from "web3";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+const web3 = new Web3(Web3.givenProvider);
+const networksData = [
+  { name: "Ethereum Mainnet", src: "/ether.png", id: "1" },
+  { name: "Binance Smart Chain", src: "/bnb.png", id: "56" },
+  { name: "Polygon (Matic)", src: "/polygon.png", id: "137" },
+];
 const MainLayout = ({ children }) => {
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  async function connectWallet() {
+    const account = await web3.eth
+      .requestAccounts()
+      .then((accounts) => accounts[0]);
+    setAccount(summarizeAddress(account));
+    networkChanger();
+  }
+
+  function summarizeAddress(address) {
+    const start = address.slice(0, 4);
+    const end = address.slice(-4);
+    return `${start}....${end}`;
+  }
+  async function networkChanger(chainId) {
+    const networkId = (await chainId) || (await web3.eth.getChainId());
+    console.log(networkId);
+    const network = networksData.find((chain) => {
+      return +chain.id === networkId;
+    });
+    setNetwork(network);
+  }
+  useEffect(() => {
+    window.ethereum.on("chainChanged", () => {
+      networkChanger();
+    });
+  }, []);
   return (
     <>
       <Head>
@@ -27,7 +62,19 @@ const MainLayout = ({ children }) => {
           </nav>
         </div>
         <div className={styles["nav-controllers"]}>
-          <div className={styles["nav-controller"]}>Connect Wallet</div>
+          <div className={styles["nav-controller"]} onClick={connectWallet}>
+            {network && (
+              <div className={styles["network"]}>
+                <Image
+                  alt="current network"
+                  width={30}
+                  height={30}
+                  src={network.src}
+                />
+              </div>
+            )}
+            {account || "Connect Wallet"}
+          </div>
         </div>
       </header>
       <main className={styles["main"]}>{children}</main>
